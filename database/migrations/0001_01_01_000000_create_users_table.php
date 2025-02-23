130f66609f6f3cdc26751ed8e103610f2a1e67fa
Otavio Prada <142634808+OtavioPrada10@users.noreply.github.com>
2025-02-23 17:08:46 -0300

    init

--- database/migrations/0001_01_01_000000_create_users_table.php
+++ database/migrations/0001_01_01_000000_create_users_table.php
@@ -19,6 +19,9 @@ return new class extends Migration
             $table->string('password');
             $table->rememberToken();
             $table->timestamps();
+            $table->string('cpf')->nullable(false);
+            $table->enum('type', ['Física', 'Jurídica'])->nullable(false);
+            $table->string('phone')->nullable();
         });
 
         Schema::create('password_reset_tokens', function (Blueprint $table) {
